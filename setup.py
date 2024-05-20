4cf7a536df2f6f1af1b80f7e34fc36c7fafdb38a
yuki takenaka <you@example.com>
2024-05-20 19:43:30 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="calculate_savings",
+    version="0.0.1",
+    author="YukiTakenaka",
+    author_email="s2222024@stu.musashino-u.ac.jp",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/author/agci",
+    project_urls={
+        "Bug Tracker": "https://github.com/author/agci",
+    },
+    classifiers=[
+        "Programming Language:: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=["calculate_savings"],
+    packages=setuptools.find_packages(where="sre"),
+    python_requires=">=3.7",
+    entry_points={"console _scripts": ["calculate_savings = calculate_savings:main"]},
+)
